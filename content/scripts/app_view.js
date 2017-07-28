576d5c275743a24de59297c31c824116b17c92fd
Mike Ebert <mike@mikeebert.net>
2017-07-27 19:50:48 -0500

    Update to new photo layout

--- content/scripts/app_view.js
+++
@@ -1,9 +0,0 @@
-var AppView = Backbone.View.extend({
-  render: function(thumbs) {
-    _.each( thumbs.models, function(thumb) {
-        $('div ul#thumbnails').append( new ThumbView({ model: thumb }).render().el )
-      });
-    var firstThumb = thumbs.models[0];
-    thumbs.select(firstThumb);
-  }
-});
